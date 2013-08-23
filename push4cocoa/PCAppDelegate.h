a3d2c1252f2070880ccbc1a3901ba5c3bfe2ca9a
lvjian700 <lvjian700@gmail.com>
2013-08-23 11:08:58 +0800

    Initial Commit

---
+++ push4cocoa/PCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PCAppDelegate.h
+//  push4cocoa
+//
+//  Created by 吕健 on 13-8-23.
+//  Copyright (c) 2013年 lv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ push4cocoa/PCViewController.h
@@ -0,0 +1,13 @@
+//
+//  PCViewController.h
+//  push4cocoa
+//
+//  Created by 吕健 on 13-8-23.
+//  Copyright (c) 2013年 lv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PCViewController : UIViewController
+
+@end
